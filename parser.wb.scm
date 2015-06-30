8362556ac4dd0551c8c0086f2053fa271bc2b946
William Baskin <mobile.wbaskin@gmail.com>
2015-06-30 13:18:12 -0400

    Creating a parse tree

--- parser.wb.scm
+++ parser.wb.scm
@@ -9,12 +9,30 @@
 (define parse
   (lambda (raw-file)
     (begin
-      (display "parse raw file \n")
-      (display (read_in_file raw-file))
-      (display "\n")
-      (error "parse not yet implmented.")
-      #t)))
+      (cond
+        (display raw-file)
+        ((null? raw-file) (error "empty input file"))
+        (else (_parse raw-file '(root) (lambda (v) v)))))))
+    
+(define _parse
+  (lambda (raw-file tree return)
+    (cond
+      ((null? raw-file) (return '()))
+      (else (_parse (cdr raw-file) (parse-one (car raw-file) tree return) (lambda (rest) (return (cons (car tree) rest))))))))
+    
+(define parse-one
+  (lambda (line tree return)
+    (cond
+      ((null? line) (return '()))
+      ((eq? (car line) 'return) 
+
+(define parse-expression
+  (lambda (line return)
+    (cond
+      ((null? line) (return '()))
+      ((eq? (length line) '1) (return line))
+      (else (error "multiple element expression parse not yet implemented")))))
 
 (define _run-parser
   (lambda (raw-file)
-    (parse raw-file)))
+    (parse (read_in_file raw-file))))
