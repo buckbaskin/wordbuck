5c1d153dee9c01c98f3f257d1ae4ec2ed0c8375c
William Baskin <mobile.wbaskin@gmail.com>
2015-07-01 00:03:22 -0400

    So, parsing is hard.

--- parser.wb.scm
+++ parser.wb.scm
@@ -24,15 +24,29 @@
   (lambda (line tree return)
     (cond
       ((null? line) (return '()))
-      ((eq? (car line) 'return) 
+      ((eq? (car line) 'return) (parse-one (cdr line) tree (lambda (rest) (list 'return (parse-expression (cdr line) (lambda (v) v)) rest)))) 
+      (else (error "parse-one: not implemented")))))
+
+(define parse-expr
+  (lambda (line)
+    (parse-expression line (lambda (v) v))))
 
 (define parse-expression
   (lambda (line return)
     (cond
       ((null? line) (return '()))
       ((eq? (length line) '1) (return line))
-      (else (error "multiple element expression parse not yet implemented")))))
+      ((eq? (length line) '2) (return line))
+      (else (parse-expression (cddr line) (lambda (rest) (append (list (operator line)) (append (list (car line)) rest))))))))
 
 (define _run-parser
   (lambda (raw-file)
     (parse (read_in_file raw-file))))
+
+(define _parse-expression
+  (lambda (raw-file)
+    (map parse-expr (read_in_file raw-file))))
+
+(define operator
+  (lambda (line)
+    (cadr line)))
