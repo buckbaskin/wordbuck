e85c6eae8151bfccb4e839d016ae08ea0d013a31
William Baskin <mobile.wbaskin@gmail.com>
2015-07-12 18:06:34 -0400

    Starting interpreter architecture

--- interpreter.wb.scm
+++ interpreter.wb.scm
@@ -8,14 +8,29 @@
 ; in -> parse tree with main method
 ; out-> program output
 
+; run tests with (_i "parser_level1/p1_test01.txt")
+
 (define interpret
   (lambda (parsed-file)
     (begin
-      (display ("parsed file \n"))
-      (display (parsed-file))
-      (error "interpret not yet implemented. \n")
-      #t)))
+      (display "parsed file \n")
+      (display parsed-file)
+      (display "\ninterpreter out:\n")
+      (process_arg_list parsed-file (new_state)))))
 
 (define _run-interpreter
   (lambda (raw-file)
     (interpret (parser raw-file))))
+
+(define _i
+  (lambda (file)
+    (_run-interpreter file)))
+
+(define process_arg_list
+  (lambda (l state)
+    (error "process_arg_list not yet implemented")))
+
+(define new_state
+  (lambda ()
+    ; ((vars) (vals))
+    '(() ())))
