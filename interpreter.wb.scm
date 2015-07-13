cbca9901f777d52826447d14fd5c3ed115066aa5
William Baskin <mobile.wbaskin@gmail.com>
2015-07-13 00:47:03 -0400

    WIP

--- interpreter.wb.scm
+++ interpreter.wb.scm
@@ -10,6 +10,14 @@
 
 ; run tests with (_i "parser_level1/p1_test01.txt")
 
+; _ leading underscore denotes a function called by other scripts
+
+(define help
+  (lambda ()
+    (begin
+      (display "This is the help text for the _wordbuck_ Interpreter")
+      #t)))
+
 (define interpret
   (lambda (parsed-file)
     (begin
@@ -39,8 +47,16 @@
 
 (define new_state
   (lambda ()
-    ; ((vars) (vals))
+    ; ((names) (objs))
     '(() ())))
+
+(define is_valid_state?
+  (lambda (state)
+    (cond
+      ((null? state) #f)
+      ((not (list? state)) #f)
+      ((eq? (length state) '2) #t)
+      (else #f))))
 
 ; ====== M Class ======
 ; return the state given a class definition
@@ -60,7 +76,46 @@
 
 (define M_state
   (lambda (arg arg_list state term return excep cont break)
-    (error "M_state not yet implemented")))
+    (cond
+      ((null? arg) (error "M_state: argument is null"))
+      ((not (is_valid_state? state)) (error "M_state: invalid state"))
+      ((is_return? arg) (M_s_return arg arg_list state term return excep cont break)))
+      (else (error "That part of M_s is not defined"))))
+
+; M_state pieces
+
+(define is_return?
+  (lambda (arg)
+    ((null? arg) #f)
+    ((not (eq? (length arg) '2)) #f)
+    (else (eq? 'return (car arg)))))
+
+(define M_s_return
+  (lambda (arg arg_list state term return excep cont break)
+    (
+
+; M_state utils
+      
+(define create_obj
+  (lambda (name state)
+    (list (cons name (car state)) (cons 'notDefined (cadr state)))))
+
+(define create ; helper to create_obj when the state is more complicated
+  (lambda (name var_list val_list return)
+    (error "create: not yet implemented")))
+
+(define assign_obj ; method is the programmer facing. Splits state, and then rebuilds state
+  (lambda (name obj state)
+    (assign name obj (car state) (cadr state) (lambda (names objs) (list names objs)))))
+     
+(define assign
+  (lambda (name obj name_list obj_list return)
+    (cond
+      ((or (null? name_list) (null? obj_list)) (error "assign_var: variable not yet initialized"))
+      ((eq? name (car name_list)) (return name_list (cons obj (cdr obj_list))))
+      (else (assign name obj (cdr name_list) (cdr obj_list) (lambda (names objs) (return (cons (car name_list) names) (cons (car obj_list) objs))))))))
+
+
 
 ; ====== M Value ======
 ; evaluate 
