e690a653361af24b19c7090a7697f26e75018af3
William Baskin <mobile.wbaskin@gmail.com>
2015-07-29 15:11:19 -0400

    Fixing test (ran to tests +1)

--- test.interpreter.wb.scm
+++ test.interpreter.wb.scm
@@ -8,17 +8,20 @@
                                                    "parser_level1\\p1_test08.txt" "parser_level1\\p1_test09.txt" "parser_level1\\p1_test10.txt") 
                   '(150 -4 10 16 220 5 6 10 5 -39)
                   '1) (begin
-                        (display " passed\nAll Tests passed\n")
+                        (display "All Tests passed\n")
                         #t))
       (else (begin (display " <<< failed test(s)\n") #f)))))
 
 (define test_list
   (lambda (files vals test_number)
-    (begin (display "test ") (display test_number)
     (cond
       ((or (null? files) (null? vals)) #t)
-      ((test_one (car files) (car vals)) (begin (display " passed\n") (test_list (cdr files) (cdr vals) (+ 1 test_number))))
-      (else (begin (display " failed\n") (test_list (cdr files) (cdr vals) (+ 1 test_number))))))))
+      (else
+       (begin (display "test ") (display test_number)
+              (cond
+                ((or (null? files) (null? vals)) #t)
+                ((test_one (car files) (car vals)) (begin (display " passed\n") (test_list (cdr files) (cdr vals) (+ 1 test_number))))
+                (else (begin (display " failed\n") (test_list (cdr files) (cdr vals) (+ 1 test_number))))))))))
 
 (define test_one
   (lambda (file expected_value)
