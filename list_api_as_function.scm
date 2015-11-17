c8d4a718a9e3614806bebcb55d96cac484f31d15
William Baskin <mobile.wbaskin@gmail.com>
2015-11-17 17:37:30 -0500

    Add clear, contains

--- list_api_as_function.scm
+++ list_api_as_function.scm
@@ -21,9 +21,20 @@
       ((null? collection) (return list))
       (else (add_all list (cdr collection) (lambda (l2) (return (cons (car collection) l2))))))))
 
+; addAll(int index, Collection<E> c)
 (define add_all_index
   (lambda (list index collection return)
     (cond
       ((<= index 0) (add_all list collection (lambda (l2) (return l2))))
       ((null? list) (return '()))
       (else (add_all_index (cdr list) (- index 1) collection (lambda (list2) (return (cons (car list) list2))))))))
+
+; clear()
+(define clear
+  (lambda (list)
+    '()))
+
+; contains(Object o)
+(define contains
+  (lambda (list object)
+    (foldl (lambda (a b) (or a b)) 'f (map (lambda (n) (eq? object n)) list))))
